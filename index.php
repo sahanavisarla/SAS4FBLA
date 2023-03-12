4af75eee849895ddee7fa1f877e4278e23e07fa0
sahanavisarla <127689442+sahanavisarla@users.noreply.github.com>
2023-03-12 14:22:57 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+  <head>
+     <style type="text/css">   
+     body
+   			{
+	   		  background-color:#ADD8E6;
+	   		  color:black;
+	   		  font-family:arial;
+	   		  font-size:25;
+	   		  text-align:center;
+   			} 
+   			table
+   			{
+	   			margin:auto;
+	   			width: 80%;
+	    		text-align:center;
+	    		color: black;
+	    		border:3px black solid;
+	   			background-color: lavender;
+   			}
+   			td
+   			{
+	   			border:3px black solid;
+	   			padding:5;
+   			}
+
+   </style>
+    <title>Marquette High School</title>
+  </head>
+  <body>
+    <h1>Welcome to Marquette High School</h1>
+    <nav>
+          <a href="login.php">Login</a>  |   
+        <a href="a href="register.php">Register</a> |  
+        <a href="attendance.php">Attendance</a> |
+<a href="grades.php">Grades</a> |  
+        <a href="points.php">Points</a> |  
+        <a href="report_card.php">Report Card</a> 
+    
+    </hr>
+    
+<table class="center">       
+     
+        <tr class="center"></tr>
+           <h1 class="table">Mission Statement</h1>
+          The mission of Marquette High School, home of the Mustangs, is to provide a safe and secure environment that will inspire, challenge, and encourage each student to develop a mature intellectual, creative, social, and physical foundation for lifelong learning in a global society. We support this through a concentrated effort of the entire Marquette family including students, staff, parents, and community.  </tr>
+    
+      </table>
+
+  <table class="center">>
+  </body>
+</html>
